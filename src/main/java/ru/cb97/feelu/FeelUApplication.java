0174d0655cb695db2edeb336113cb9446e1733d0
79298 <9298193414v@gmail.com>
2026-07-29 20:51:38 +0300

    Initial commit

---
+++ src/main/java/ru/cb97/feelu/FeelUApplication.java
@@ -0,0 +1,13 @@
+package ru.cb97.feelu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelUApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeelUApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/cb97/feelu/FeelUApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cb97.feelu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelUApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
